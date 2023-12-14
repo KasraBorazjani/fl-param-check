0372fd6e0d0c68b8b41139e6ac9483225ba9874e
Kasra Borazjani <kasra.borazjani@gmail.com>
2023-12-13 22:20:10 -0500

    nogpu

--- array_run.py
+++ array_run.py
@@ -5,4 +5,4 @@ import numpy as np
 
 for sgd_per_round in np.arange(1, 10):
     for model_inertia in np.arange(0.1, 1, 0.1):
-        subprocess.run(["python", "train.py", "--model_inertia", str(model_inertia), "--sgd_per_round", str(sgd_per_round), "--result_path", "./results/result2"])
+        subprocess.run(["python", "train.py", "--model_inertia", str(model_inertia), "--sgd_per_round", str(sgd_per_round), "--result_path", "./results/result3", "--acc_used", "False"])
