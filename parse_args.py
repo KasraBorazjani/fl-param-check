a562c39646f937827ffaced59f4af2ecec08d55a
Kasra Borazjani <kasra.borazjani@gmail.com>
2023-12-13 22:11:49 -0500

    slurm-debuged

--- parse_args.py
+++ parse_args.py
@@ -10,13 +10,13 @@ def parse_arguments():
     
     ## Training Param Args ##
     parser.add_argument('--sgd_per_round', type=int, default=5, help='number of local epochs per federated round (default: 1)')
-    parser.add_argument('--num_clients', type=int, default=50, help='number of clients simulated (default: 50)')
+    parser.add_argument('--num_clients', type=int, default=30, help='number of clients simulated (default: 50)')
     parser.add_argument('--neighbors_per_client', type=int, default=5, help='number of clients simulated (default: 50)')
     parser.add_argument('--model_inertia', type=float, default=0.05, help='inertia for model aggregation (default: 50)')
 
 
     ## Dataset Args ##
-    parser.add_argument('--total_ds_len', type=int, default=500, help='total dataset length for each client (default: 200)')
+    parser.add_argument('--total_ds_len', type=int, default=300, help='total dataset length for each client (default: 200)')
     parser.add_argument('--primary_label_fraction', type=float, default=0.75, help="fraction of each client's dataset that is occupied by the preferred label (default: 0.75)")
     parser.add_argument('--num_secondaries', type=int, default=1, help='number of secondary labels (default: 1)')
     parser.add_argument('--batch_size', type=int, default=16, help='batch size (default: 16)')
@@ -25,7 +25,7 @@ def parse_arguments():
     ## Runtime Args ##
     parser.add_argument('--acc_used', action='store_true', default=True, help='whether acceleration (GPU/MPS) is used (default: False)')
     parser.add_argument('--random_seed', type=int, default=42, help='random seed - set manually to adapt repeatability (default: 42)')
-    parser.add_argument('--num_fed_rounds', type=int, default=100, help='maximum number of federated rounds to run the code for (default: 100)')
+    parser.add_argument('--num_fed_rounds', type=int, default=70, help='maximum number of federated rounds to run the code for (default: 100)')
     parser.add_argument('--data_path', type=str, default="../data/mnist", help='directory for data to be loaded from')
     parser.add_argument('--saved_model_path', type=str, default="../saved_models", help='directory for the saved models to be loaded from')
     parser.add_argument('--init_lr', type=float, default=1e-2, help='Initial learning rate (default: 1e-2)')
