5e0685a299a47a68ca822217360c8f37998be8d0
Kasra Borazjani <kasra.borazjani@gmail.com>
2023-12-13 21:24:48 -0500

    slurmed

--- array_run.py
+++ array_run.py
@@ -1,8 +1,8 @@
 import subprocess
 import os
+import numpy as np
 
 
-
-for sgd_per_round in range(1, 10):
-    for model_inertia in range(0.1, 1, 0.1):
+for sgd_per_round in np.arange(1, 10):
+    for model_inertia in np.arange(0.1, 1, 0.1):
         subprocess.run(["python", "train.py", "--model_inertia", model_inertia, "--sgd_per_round", sgd_per_round, "--result_path", "./results/result1"])
